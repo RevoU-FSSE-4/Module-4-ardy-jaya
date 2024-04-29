b5320aff739fae65886cc27b117c7ec4d729d92c
ardy-jaya <ardy.henjaya@gmail.com>
2024-04-29 21:51:34 +0700

    update tugas

---
+++ signin-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './homepage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     <HomePage/>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
