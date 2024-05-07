4ade1f80f79d72a5145c213199d6e6e5de84d803
ardy-jaya <ardy.henjaya@gmail.com>
2024-05-07 16:56:30 +0700

    update private route

--- signin-app/src/privateRouter.tsx
+++ signin-app/src/privateRouter.tsx
@@ -1,13 +1,9 @@
-
-
 import { Navigate, Outlet, Route } from "react-router-dom";
 import React from "react";
 
-
 const PrivateRoute: React.FC = () => {
-    const islogin = localStorage.getItem('islogin') === 'true';
-    return islogin ? <Outlet /> : <Navigate to="/signin" replace />;
-}; 
-
+  const islogin = localStorage.getItem("token") === "true";
+  return islogin ? <Outlet /> : <Navigate to="/signin" replace />;
+};
 
 export default PrivateRoute;
